9c6ebdab84344725913ab7f8b11f64f13c592579
Markus Metz <markus.metz.giswork@gmail.com>
2020-02-13 20:50:03 +0100

    DGM import
    avoid sed, use reference raster if available, use r.buildvrt if available

--- dgm/r.in.opennrw_dgm_epgs25832.sh
+++ dgm/r.in.opennrw_dgm_epgs25832.sh
@@ -51,6 +51,10 @@
 # debug
 # zip=dgm1_05154020_Issum_EPSG4647_XYZ.zip
 
+# optional reference raster for grid geometry alignment
+# NOTE: this can introduce a grid shift
+ref_raster=""
+
 if  [ -z "$GISBASE" ] ; then
  echo "You must be in GRASS GIS to run this program." >&2
  exit 1
@@ -62,10 +66,10 @@ if [ ! -x "`which fuse-zip`" ] ; then
     exit 1
 fi
 
-#check if a MASK is already present:
+#check if a MASK is already present in the current mapset:
 MASKTMP=mask.$$
 USERMASK="usermask_$MASKTMP"
-eval `g.findfile element=cell file=MASK`
+eval `g.findfile element=cell file=MASK mapset=.`
 if [ "$file" ] ; then
     g.message "A user raster mask (MASK) is present. Saving it..."
     g.rename raster=MASK,"$USERMASK" --quiet > /dev/null
@@ -95,15 +99,20 @@ for zip in `ls dgm*_EPSG4647_XYZ.zip` ; do
   for dgm in `ls dgm*.xyz` ; do
     # irregular spaces :(
     # to get rid of them, we pipe the entire XYZ-DGM into the "tr", then into r.in.xyz used as bounding box scanner
-    compregion=`cat $dgm  | tr -s ' ' ' ' | sed 's+^32++g' | r.in.xyz input=- separator=space -s -g output=dummy | cut -d' ' -f1-`
+    compregion=`cat $dgm  | tr -s ' ' ' ' | r.in.xyz input=- separator=space -s -g output=dummy | cut -d' ' -f1-4`
     g.region $compregion res=1 -p
 
     # enlarge computational region by half a raster cell (here 0.5m) to
     # store the points as cell centers:
     g.region n=n+0.5 s=s-0.5 w=w-0.5 e=e+0.5 -p
+    if [ -n "$ref_raster" ] ; then
+      g.region align=$ref_raster -p
+    fi
 
     name=`basename $dgm .xyz`
-    cat $dgm | tr -s ' ' ' ' | sed 's+^32++g' | r.in.xyz input=- separator=space method=mean output=$name
+    cat $dgm | tr -s ' ' ' ' | r.in.xyz input=- separator=space method=mean output=$name
+    # shift from EPSG:4647 to EPSG:25832, adjusting false easting
+    r.region map=$name e=e-32000000 w=w-32000000
     
   done  # end of tile loop
 
@@ -118,23 +127,27 @@ for zip in `ls dgm*_EPSG4647_XYZ.zip` ; do
   g.region raster=$TILELIST -p
   
   # merge all tiles into one map
-  r.patch input=$TILELIST output=$xyz
-  
+  if [ -x "`which r.buildvrt`" ] ; then
+      r.buildvrt input=$TILELIST output=$xyz
+  else
+      r.patch input=$TILELIST output=$xyz
+  fi
+
   # write out merged DGM mosaik as a compressed GeoTIFF
   r.out.gdal -m -c input=$xyz output=${xyz}_epsg25832.tif type=Float32 createopt="COMPRESS=LZW"
   r.pack input=${xyz} output=${xyz}_epsg25832.pack
 
-  # cleanup: delete all imported tiles
-  g.remove raster pattern="dgm*" -f
   # cleanup: delete mosaik
   g.remove raster pattern=$xyz -f
+  # cleanup: delete all imported tiles
+  g.remove raster pattern="dgm*" -f
 
   # at this stage the mapset should be empty and the zip file disconnected. Clean for the next loop...
   
 done # end of city loop
 
 #restore user mask if it was present:
-eval `g.findfile element=cell file=$USERMASK`
+eval `g.findfile element=cell file=$USERMASK mapset=.`
 if [ "$file" ] ; then
   g.message "Restoring user raster mask (MASK) ..."
   g.remove raster name=MASK -f --quiet > /dev/null
