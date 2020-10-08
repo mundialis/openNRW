51bd401ae6cdcad5033f2beacf5a0ba3dc319287
Markus <neteler@gmail.com>
2020-10-08 11:24:52 +0200

    DGM1 processing: update to current file structure

---
+++ dgm/01_grass_import_openNRW_dgm1_epsg25832.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+############################################################################
+#
+# NAME:         01_grass_import_opennrw_dgm1_epsg25832.sh
+#
+# AUTHOR(S):    Markus Neteler <neteler at mundialis.de>
+#               mundialis GmbH & Co. KG, Bonn
+#               https://www.mundialis.de
+#
+# PURPOSE:      Script to import openNRW DGM XYZ data as regularly gridded DGMs
+#               It loops over all "Gemeinden" DGM-ZIPs saved in a directory.
+#
+#               Note: It removes the leading "32" from East coordinate (EPSG4647 --> 25832 hack)
+#                     because EPSG4647 comes with false Easting extended by 32000000 to get the preceeding 32.
+#                     Hence 4647 stores with preceeding zone number, whereas 25832 does not.
+#                     So 25832 coordinate xxxxxx is 32xxxxxx in 4647.
+#               See also:
+#                 Maßgeschneiderte EPSG-Codes für GIS-Anwendungen https://www.zentrale-stelle-sapos.de/files/EPSG-Codes.pdf
+#
+# COPYRIGHT:    (C) 2017-2020 by Markus Neteler, mundialis
+#
+# REQUIREMENTS: apt-get install fuse-zip (Ubuntu/Debian)
+#               dnf install fuse-zip     (Fedora/CentOS)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+############################################################################
+
+#
+# openNRW data source:
+# https://www.opengeodata.nrw.de/produkte/geobasis/hm/dgm1_xyz/
+
+####
+# first time only, create location:
+# grass78 -c epsg:25832 -e ~/grassdata/openNRW_epsg25832/
+#
+####
+#
+# for all subsequent uses, start GRASS GIS for the DGM session:
+# grass78 ~/grassdata/openNRW_epsg25832/PERMANENT/
+
+# debug
+# zip=dgm1_05154020_Issum_EPSG25832_XYZ.zip
+
+# optional reference raster for grid geometry alignment
+# NOTE: this can introduce a grid shift
+ref_raster=""
+
+# are we in GRASS GIS? https://grass.osgeo.org/
+if  [ -z "$GISBASE" ] ; then
+ echo "You must be in GRASS GIS to run this program." >&2
+ exit 1
+fi
+
+#### check if we have fuse-zip
+if [ ! -x "$(which fuse-zip)" ] ; then
+    g.message -e "fuse-zip required, please install fuse-zip first"
+    exit 1
+fi
+
+export GRASS_MESSAGE_FORMAT=plain
+
+#check if a MASK is already present in the current mapset:
+MASKTMP=mask.$$
+USERMASK="usermask_$MASKTMP"
+eval $(g.findfile element=cell file=MASK mapset=.)
+if [ "$file" ] ; then
+    g.message "A user raster mask (MASK) is present. Saving it..."
+    g.rename raster=MASK,"$USERMASK" --quiet > /dev/null
+fi
+
+# loop over all openNRW dgm1 city packages a.k.a. ZIP files (outer loop)
+# "Paketierung: Gemeinden"
+for zip in $(ls dgm*_EPSG25832_XYZ.zip) ; do
+
+    # process DGM1 of current city in the list
+    xyz=$(basename $zip _EPSG25832_XYZ.zip)
+    echo "Processing $xyz..."
+
+    # mount ZIP file (instead of unpacking it which would double the disk space consumption)
+    ZIPDIR=$(basename ${zip} .zip)
+    mkdir -p ${ZIPDIR}
+    fuse-zip ${zip} ${ZIPDIR}
+    cd ${ZIPDIR}
+
+    # loop over all DGM files in zip file
+    # concept:
+    # 1. scan all gridded points in XYZ-DGM to obtain the bounding box (since it is unknown)
+    # 2. set computational region to bbox +0.5m in all directions (points must fall into cell center, not corner)
+    # 3. import points as raster maps
+    #
+    # one city ZIP file contains many xyz tiles files... (inner loop)
+    for dgm in $(ls dgm*.xyz) ; do
+        # irregular spaces found in a few files :(
+        # to get rid of them, we pipe the entire XYZ-DGM into the "tr", then into r.in.xyz used as bounding box scanner
+        compregion=$(cat $dgm  | tr -s ' ' ' ' | r.in.xyz input=- separator=space -s -g output=dummy | cut -d' ' -f1-4)
+        g.region $compregion res=1 -p
+
+        # enlarge computational region by half a raster cell (here 0.5m) to
+        # store the points as cell centers:
+        g.region n=n+0.5 s=s-0.5 w=w-0.5 e=e+0.5 -p
+        if [ -n "$ref_raster" ] ; then
+           g.region align=$ref_raster -p
+        fi
+
+        name=$(basename $dgm .xyz)
+        cat $dgm | tr -s ' ' ' ' | r.in.xyz input=- separator=space method=mean output=$name
+        # shift from EPSG:4647 to EPSG:25832, adjusting false easting
+        r.region map=$name e=e-32000000 w=w-32000000
+    done  # end of tile loop
+
+    # move out of city ZIP file
+    cd ..
+    # import done, unmount ZIP file
+    fusermount -u ${ZIPDIR} && rmdir ${ZIPDIR}
+  
+    # generate list of all tiles which are now in the GRASS GIS location/mapset
+    TILELIST=$(g.list raster pattern="dgm*" sep=comma)
+    # set computational region to all tiles as prep for overall DGM of city
+    g.region raster=$TILELIST -p
+    # merge all tiles into one virtual map, in case VRT support is included in GRASS GIS version (7.8+)
+    if [ -x "$(which r.buildvrt)" ] ; then
+       r.buildvrt input=$TILELIST output=$xyz
+    else
+       r.patch input=$TILELIST output=$xyz
+    fi
+
+    # write out merged DGM mosaik as a compressed GeoTIFF, with overviews
+    # (compress GeoTIFF overviews as well)
+    export COMPRESS_OVERVIEW="LZW"
+    r.out.gdal -m -c input=$xyz output=${xyz}_epsg25832.tif type=Float32 createopt="COMPRESS=LZW,TILED=YES" overview=5
+    r.pack input=${xyz} output=${xyz}_epsg25832.pack
+
+    # cleanup: delete mosaik
+    g.remove raster pattern=$xyz -f
+    # cleanup: delete all imported tiles
+    g.remove raster pattern="dgm*" -f
+
+    # at this stage the mapset should be empty and the zip file disconnected - cleaned for the next loop cycle...
+  
+done # end of openNRW city loop ("Paketierung: Gemeinden")
+
+#restore user mask if it was present:
+eval $(g.findfile element=cell file=$USERMASK mapset=.)
+if [ "$file" ] ; then
+  g.message "Restoring user raster mask (MASK) ..."
+  g.remove raster name=MASK -f --quiet > /dev/null
+  g.rename raster="$USERMASK",MASK --quiet > /dev/null
+fi
+
+exit 0
--- dgm/02_dgm1_extract_bonn_DEM_mosaik_procedure.sh
+++ dgm/02_dgm1_extract_bonn_DEM_mosaik_procedure.sh
@@ -2,13 +2,15 @@
 
 ############################################################################
 #
-# NAME:         dgm1_extract_bonn_DEM_mosaik_procedure.sh
+# NAME:         02_dgm1_extract_bonn_DEM_mosaik_procedure.sh
 #
 # AUTHOR(S):    Markus Neteler <neteler at mundialis.de>
 #               mundialis GmbH & Co. KG, Bonn
 #               https://www.mundialis.de
 #
-# PURPOSE:      Script to mosaik all openNRW dgm1 tiles which cover the city of Bonn
+# PURPOSE:      Script to mosaik all openNRW dgm1 tiles which cover an area (here: city of Bonn)
+#
+# REQUIREMENT:  All DGM1 files have been imported. See 01_grass_import_opennrw_dgm1_epsg25832.sh
 #
 # COPYRIGHT:    (C) 2018-2020 by Markus Neteler, mundialis
 #
@@ -26,43 +28,47 @@
 
 ## Example: Bonn
 #
-# openNRW data source:
+# openNRW DGM1 data source:
 # https://www.opengeodata.nrw.de/produkte/geobasis/hm/dgm1_xyz/
 
-# fetch Bonn city boundary
-# see http://training.gismentors.eu/grass-gis-workshop-jena-2018/units/02.html?highlight=openstreetmap
-ogr2ogr -f GPKG bonn_boundary_osm_epsg4647.gpkg -a_srs EPSG:4647 -t_srs EPSG:25832 /vsicurl_streaming/"http://overpass-api.de/api/interpreter?data=%28relation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%3D%226%22%5D%5B%22name%22%3D%22Bonn%22%5D%3B%29%3B%28%2E%5F%3B%3E%3B%29%3Bout%3B%0A" multipolygons
+# fetch Bonn city boundary from OpenStreetMap
+# see https://training.gismentors.eu/grass-gis-workshop-jena/units/02.html?highlight=openstreetmap
+ogr2ogr -f GPKG bonn_boundary_osm_epsg25832.gpkg -a_srs EPSG:4326 -t_srs EPSG:25832 /vsicurl_streaming/"http://overpass-api.de/api/interpreter?data=%28relation%5B%22boundary%22%3D%22administrative%22%5D%5B%22admin%5Flevel%22%3D%226%22%5D%5B%22name%22%3D%22Bonn%22%5D%3B%29%3B%28%2E%5F%3B%3E%3B%29%3Bout%3B%0A" multipolygons
 
-grass74 -c epsg:4647 ~/grassdata/epsg4647_openNRW/
+# start GRASS GIS
+grass78 -c epsg:25832 ~/grassdata/openNRW_eps25832/
+
+# import city boundary from OSM
 v.import bonn_boundary_osm_epsg25832.gpkg out=bonn_boundary_osm
 
-# set computational region to vector map but align pixel geometry to DGM (any related dgm1 is fine)
+# set computational region to vector map but align pixel geometry to DGM (any related dgm1 map is fine)
 g.region vector=bonn_boundary_osm align=dgm1_32330_5712_2_nw -p
 
 # count DEMs
 g.list raster pattern="dgm1_3*" > list.csv
 wc -l list.csv
-# 9275 list.csv
+# 9275 list.csv in total
 
 # Now easily identify DGM tiles covering the city of Bonn, using current region:
 g.list raster pattern="dgm1_3*" region=.
 g.list raster pattern="dgm1_3*" region=. | wc -l
-# 63 tiles
+# 63 tiles covering Bonn
 
-# Use that list
-r.patch input=`g.list raster pattern="dgm1_3*" sep=comma region=.` output=dgm1_bonn
+# Use that list to create a virtual mosaic
+r.buildvrt input=$(g.list raster pattern="dgm1_3*" sep=comma region=.) output=dgm1_bonn
 r.colors dgm1_bonn color=elevation
 
 # take a look
 d.mon wx0
+# visualize DGM1 of Bonn
 d.rast dgm1_bonn
 d.vect bonn_boundary_osm type=boundary
 
-# shaded relief
+# compute shaded relief
 r.relief input=dgm1_bonn output=dgm1_bonn_shaded
-
-# geomorphons
+# compute geomorphons
 r.geomorphon elevation=dgm1_bonn forms=dgm1_bonn_geomorphon 
 
+# visualize geomorphons, shaded with terrain
 d.erase
 d.shade shade=dgm1_bonn_shaded color=dgm1_bonn_geomorphon 
